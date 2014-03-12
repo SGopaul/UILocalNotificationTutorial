2d310b8a87e6a1e185223218f34fa675ae322e90
stephangopaul <stephangopaul@gmail.com>
2014-03-12 12:21:17 +0400

    Create project

---
+++ UILocalNotification/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UILocalNotification
+//
+//  Created by Stephan Gopaul on 12/03/2014.
+//  Copyright (c) 2014 Stephan Gopaul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UILocalNotification/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UILocalNotification
+//
+//  Created by Stephan Gopaul on 12/03/2014.
+//  Copyright (c) 2014 Stephan Gopaul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
